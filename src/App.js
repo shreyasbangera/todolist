bf64a33c575dd477ac3ff2a6daee3b0f237af7d1
shreyasbangera <shreyasybangera1@gmail.com>
2023-01-17 18:30:36 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [todo, setTodo] = useState("");
+  const [todos, setTodos] = useState([
+    { id: 1, todo: "Pay Bills" },
+    { id: 2, todo: "Meet George" },
+    { id: 3, todo: "Buy eggs" },
+    { id: 4, todo: "Read a book" },
+    { id: 5, todo: "Organize office" },
+  ]);
+
+  const handleAdd = (e) => {
+    if (todo === "") {
+      alert("You must write something!");
+    } else {
+      setTodos([...todos,{ id: `${todo}-${Date.now()}`, todo }]);
+      setTodo("");
+    }
+  };
+
+  const handleClose = (id) => {
+    const delTodo = todos.filter((t) => t.id !== id);
+    console.log(delTodo);
+    setTodos(delTodo);
+  };
+
+  const handleChecked = (e) => {
+    e.target.classList.toggle("checked");
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="header">
+        <h2>My To Do List</h2>
+        <input
+          type="text"
+          id="myInput"
+          placeholder="Title..."
+          value={todo}
+          onChange={(e) => setTodo(e.target.value)}
+        />
+        <span onClick={handleAdd} className="addBtn">
+          Add
+        </span>
+      </div>
+
+      <div>
+        {todos.map((t) => (
+          <ul key={t.id} onClick={handleChecked}>
+            <li className="todo">
+              {t.todo}
+              <span onClick={() => handleClose(t.id)} className="close">
+                x
+              </span>
+            </li>
+          </ul>
+        ))}
+      </div>
+    </>
   );
 }
 
